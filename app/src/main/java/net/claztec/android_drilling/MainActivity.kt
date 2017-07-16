26503011f7db5c1482b2affc27493256c81b7004
Derek <claztec@gmail.com>
2017-07-16 17:15:01 +0900

    버튼클릭시 에니메이션 추가

--- app/src/main/java/net/claztec/android_drilling/MainActivity.kt
+++ app/src/main/java/net/claztec/android_drilling/MainActivity.kt
@@ -1,32 +1,45 @@
 package net.claztec.android_drilling
 
-import android.animation.ObjectAnimator
-import android.support.v7.app.AppCompatActivity
+import android.animation.Animator
+import android.animation.AnimatorInflater
 import android.os.Bundle
-import android.util.Log
+import android.support.v7.app.AppCompatActivity
 import android.view.View
-import android.view.animation.Animation
-import android.view.animation.AnimationUtils
-import android.widget.TextView
+import android.view.animation.AnimationSet
+import android.widget.Button
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
     val TAG = MainActivity::class.java.name
 
-    lateinit var textMessage: TextView
+    lateinit var dayViewButton: Button
+    lateinit var nightViewButton: Button
+    lateinit var sunImage: ImageView
+    lateinit var moonImage: ImageView
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        textMessage = findViewById(R.id.textView)
+
+        dayViewButton = findViewById(R.id.day)
+        nightViewButton = findViewById(R.id.night)
+        sunImage = findViewById(R.id.sun)
+        moonImage = findViewById(R.id.moon)
+
+        dayViewButton.setOnClickListener {
+            sunImage.visibility = View.VISIBLE
+            val sunRise: Animator? = AnimatorInflater.loadAnimator(applicationContext, R.animator.sun_swing)
+            sunRise?.setTarget(sunImage)
+            sunRise?.start()
+            moonImage.visibility = View.VISIBLE
+            val moonFail: Animator? = AnimatorInflater.loadAnimator(applicationContext, R.animator.moon_swing_away)
+            moonFail?.setTarget(moonImage)
+            moonFail?.start()
+        }
 
 
-        textMessage.visibility = View.VISIBLE
-
-        val anim: ObjectAnimator = ObjectAnimator.ofFloat(textMessage, "rotationY", 0.0F, 180.0F)
-        anim.duration = 3000
-        anim.start()
 
 
     }
